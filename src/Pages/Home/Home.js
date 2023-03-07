28e7f38c49ef4392560316fcd8fcf50deb981067
sushant-hire <hiresushant@yahoo.com>
2023-03-07 11:56:33 +0530

    Final Project 5

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,23 +8,48 @@ import jsPDF from "jspdf";
 import ImageIcon from "@mui/icons-material/Image";
 import html2canvas from "html2canvas";
 
+// This is a functional component named "Home"
+// It initializes two state variables, "zoomLevel" and "image", using the "useState" hook.
+// It also initializes a ref variable "fileInputRef" using the "useRef" hook.
 function Home() {
-  const [zoomLevel, setZoomLevel] = useState(100);
+  // State
+  const [zoomLevel, setZoomLevel] = useState(100); // Initializes "zoomLevel" state variable with value 100 and "setZoomLevel" function to update "zoomLevel"
+  const [image, setImage] = useState(null); // Initializes "image" state variable with value null and "setImage" function to update "image"
 
+  // Refs
+  const fileInputRef = useRef(null); // Initializes "fileInputRef" ref variable with null value
+
+  // Functions
+
+  // This function is called when the user clicks on the "Increase font size" button. It increases the font size of the selected text by executing the "fontSize" command with a value of "7".
   function handleIncreaseFontSize() {
     document.execCommand("fontSize", false, "7");
   }
 
+  // This function is called when the user clicks on the "Decrease font size" button. It decreases the font size of the selected text by executing the "fontSize" command with a value of "3".
   function handleDecreaseFontSize() {
     document.execCommand("fontSize", false, "3");
   }
 
-  const fileInputRef = useRef(null);
-  const [image, setImage] = useState(null);
+  // This function is called when the user clicks on any of the icons. It executes the command associated with the icon.
+  function handleClick(element) {
+    document.execCommand(`${element.iconFunctionality}`);
+  }
+
+  // This function is called when the user selects a color from the color picker. It changes the color of the selected text by executing the "foreColor" command with the selected color.
+  function handleColorChange(color) {
+    document.execCommand("foreColor", false, color);
+  }
+
+  // This function is called when the user selects a background color from the color picker. It changes the background color of the selected text by executing the "backColor" command with the selected color.
+  function handleBackColorChange(color) {
+    document.execCommand("backColor", false, color);
+  }
+
+  // This function is called when the user uploads an image. It reads the uploaded file using the "FileReader" API and creates an "img" element with the uploaded image. It then inserts the image at the current cursor position using the "insertNode" method of the "Range" API.
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
-    reader.readAsDataURL(file);
     reader.onloadend = () => {
       const img = document.createElement("img");
       img.src = reader.result;
@@ -34,72 +59,36 @@ function Home() {
       range.insertNode(img);
       range.collapse(false);
     };
+
+    reader.readAsDataURL(file);
   };
 
+  // This function is called when the user clicks on the "Add Image" button. It triggers a click on the hidden "input" element used for uploading images.
   const handleImageClick = () => {
     fileInputRef.current.click();
   };
 
-  function handleClick(element) {
-    document.execCommand(`${element.iconFunctionality}`);
-  }
-
-  function handleColorChange(color) {
-    document.execCommand("foreColor", false, color);
-  }
-
-  function handleBackColorChange(color) {
-    document.execCommand("backColor", false, color);
-  }
-
-  //   function handleDownload() {
-  //     const sheetContent = document.querySelector(`.${styles.sheet}`).innerHTML;
-  //     const pdfDoc = new jsPDF();
-  //     pdfDoc.text(sheetContent, 10, 10);
-  //     pdfDoc.save("document.pdf");
-  //   }
-
+  // This function is called when the user clicks on the "Download" button. It converts the content of the "TextSheet" element to an image using the "html2canvas" library. It then creates a PDF document using the "jsPDF" library and adds the image to the document. Finally, it downloads the document as a PDF file.
   async function handleDownload() {
     const sheetContent = document.querySelector(`.${styles.TextSheet}`);
-    const canvas = await html2canvas(sheetContent, { dpi: 300 });
-    const imageData = canvas.toDataURL("image/png", 1.0);
+    const canvas = await html2canvas(sheetContent, { dpi: 300 }); // Uses html2canvas library to convert sheetContent to an image with 300 DPI resolution
+    const imageData = canvas.toDataURL("image/png", 1.0); // Converts the canvas image to a data URL with PNG format and 1.0 quality
     const pdfDoc = new jsPDF({
+      // Creates a new jsPDF object with portrait orientation, A4 size and no compression
       orientation: "portrait",
       unit: "mm",
       format: "a4",
       compress: false,
     });
-    pdfDoc.addImage(imageData, "PNG", 0, 0, 210, 297, "", "FAST");
-    pdfDoc.save("document.pdf");
+    pdfDoc.addImage(imageData, "PNG", 0, 0, 210, 297, "", "FAST"); // Adds the image to the PDF document with a width of 210mm and height of 297mm (A4 size)
+    pdfDoc.save("document.pdf"); // Saves the PDF document as "document.pdf"
   }
 
   return (
     <div className={styles.MainContainer}>
+      {/* Icons and dropdowns */}
       <div className={styles.IconContainer}>
-        <select
-          value={zoomLevel}
-          onChange={(e) => setZoomLevel(parseInt(e.target.value))}
-          className={styles.ZoomDropdown}
-        >
-          <option className={styles.ZoomOption} value={50}>
-            50%
-          </option>
-          <option className={styles.ZoomOption} value={75}>
-            75%
-          </option>
-          <option className={styles.ZoomOption} value={90}>
-            90%
-          </option>
-          <option className={styles.ZoomOption} value={100}>
-            100%
-          </option>
-          <option className={styles.ZoomOption} value={125}>
-            125%
-          </option>
-          <option className={styles.ZoomOption} value={150}>
-            150%
-          </option>
-        </select>
+        {/* Map through icons and render buttons */}
         {iconCollective.map((element, index) => (
           <button
             onClick={() => handleClick(element)}
@@ -109,6 +98,19 @@ function Home() {
             {element.iconName}
           </button>
         ))}
+        {/* Zoom level dropdown */}
+        <select
+          value={zoomLevel}
+          onChange={(e) => setZoomLevel(parseInt(e.target.value))}
+          className={styles.ZoomDropdown}
+        >
+          {[50, 75, 90, 100, 125, 150].map((value) => (
+            <option className={styles.ZoomOption} value={value} key={value}>
+              {`${value}%`}
+            </option>
+          ))}
+        </select>
+        {/* Text size increase/decrease icons */}
         <div className={styles.TextSizeIconsContainer}>
           <TextIncreaseRoundedIcon
             className={styles.TextIncreaseIcon}
@@ -119,17 +121,21 @@ function Home() {
             onClick={handleDecreaseFontSize}
           />
         </div>
+        {/* Text color input box */}
         <input
           type="color"
           onChange={(event) => handleColorChange(event.target.value)}
           className={styles.TextColorInputBox}
         />
+        {/* Text fill input box */}
         <input
           type="color"
           onChange={(event) => handleBackColorChange(event.target.value)}
           className={styles.TextFillInputBox}
         />
+        {/* Image icon */}
         <ImageIcon className={styles.ImageIcon} onClick={handleImageClick} />
+        {/* Image upload input */}
         <input
           type="file"
           accept="image/*"
@@ -140,20 +146,21 @@ function Home() {
         />
       </div>
 
+      {/* Text sheet container */}
       <div
         className={styles.TextSheetContainer}
         style={{
           transform: `scale(${zoomLevel / 100})`,
-          marginTop:
-            zoomLevel > 125 ? "20rem" : zoomLevel > 100 ? "10rem" : "0",
+          marginTop: `${zoomLevel > 125 ? 20 : zoomLevel > 100 ? 10 : 0}rem`,
         }}
       >
         <div
           className={styles.TextSheet}
-          contentEditable="true"
+          contentEditable={true}
+          suppressContentEditableWarning={true}
           style={{ fontSize: "17px" }}
         >
-          {" "}
+          {/* Image container */}{" "}
           <div>
             {image ? (
               <img
@@ -167,6 +174,8 @@ function Home() {
           </div>
         </div>
       </div>
+
+      {/* Download icon */}
       <div onClick={handleDownload} className={styles.DownloadIconContainer}>
         <DownloadRoundedIcon className={styles.DownloadIcon} />
       </div>
